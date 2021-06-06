877b4bd36571bdb5f37331eced253df67720efdd
Mitesh <miteshsajare@gmail.com>
2021-06-06 19:33:36 +0530

    mitesh

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>this is the component creat by mitesh</p>
+        </div>
+    )
+}
